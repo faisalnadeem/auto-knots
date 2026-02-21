8f1c7edbaca2efd9633182f6b23c40b005fdc93d
Faisal Nadeem <faisalnadeem@hotmail.co.uk>
2026-02-21 16:07:55 +0000

    initial commit

---
+++ AutoKnots/AutoKnots/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoKnots.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ AutoKnots/AutoKnots/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoKnots.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
